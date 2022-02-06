78dc4617ce383097e203bd7bdb77f7a5ec5c73d6
GVSU-Romandy-Vu <vuro@mail.gvsu.edu>
2022-02-06 11:52:57 -0500

    Revised Test case name, so that there can be more test cases

--- TwoPlayerBG/src/BattleshipLogicTest.java
+++ TwoPlayerBG/src/BattleshipLogicTest.java
@@ -19,7 +19,7 @@ public class BattleshipLogicTest {
 	public void testConstructor(){
 		BattleshipLogic game = new BattleshipLogic();
 		int[][] player1board = game.getBoard(1);
-		int[][]player2board = game.getBoard(2);
+		int[][] player2board = game.getBoard(2);
 		
 		for(int i = 0; i < 10; i++) {
 			for(int j = 0; j < 10; j++) {
@@ -33,32 +33,42 @@ public class BattleshipLogicTest {
 	
 	@Test
 	/*********************************************************************************
-	 * Test placing ships
+	 * Test valid placing ships for player 1
 	 ********************************************************************************/
-	public void testPlayer1PlaceShips() {
+	public void testValidPlayer1PlaceShips() {
 		BattleshipLogic game = new BattleshipLogic();
-		//Valid player 1 placement of patrol boat
+		//Valid placement of patrol boat
 		assertTrue(game.placeShip(1, "A1", "A2", 2));
-		//Invalid  player 1 placement of submarine due to invalid coordinates.
-		assertFalse(game.placeShip(1,"dssdasd","V7",3));
-		//Invalid player 1 placement of Cruiser due to spot already taken.
-		assertFalse(game.placeShip(1, "A1", "C1", 3));
-		//Invalid player 1 placement of Carrier due to coordinates that aren't in board.
-		assertFalse(game.placeShip(1, "K9", "K14", 5));
-		//Invalid player 1 placement of destroyer due to invalid coordinate difference.
-		assertFalse(game.placeShip(1, "B1", "B3", 4));
+		//Valid placement of Submarine
+		assertTrue(game.placeShip(1, "B1", "D1", 3));
+		//Valid placement of Cruiser
+		assertTrue(game.placeShip(1, "B2", "B4", 3));
+		//Valid placement of Destroyer
+		assertTrue(game.placeShip(1, "A5", "D5", 4));
+		assertTrue(game.placeShip(1, "A10","E10",5));
 		
+		assertTrue(game.getCoordinateData("a01", 1) == 1);
+		assertTrue(game.getCoordinateData("A2", 1) == 1);
 		
-		int[][] player1board = game.getBoard(1);
-		//Coordinate of patrol boats
-		assertTrue(player1board[0][0] == 1);
-		assertTrue(player1board[0][1] == 1);
-		//Cruiser should not been placed.
-		assertFalse(player1board[1][0] == 1);
-		assertFalse(player1board[2][0] == 1);
-		//Check if destroyer placed (should be false)
-		assertFalse(player1board[1][0] == 1);
-		assertFalse(player1board[1][1] == 1);
+		assertTrue(game.getCoordinateData("B1", 1) == 1);
+		assertTrue(game.getCoordinateData("c01", 1) == 1);
+		assertTrue(game.getCoordinateData("D1", 1) == 1);
+		
+		assertTrue(game.getCoordinateData("B02", 1) == 1);
+		assertTrue(game.getCoordinateData("b3", 1) == 1);
+		assertTrue(game.getCoordinateData("B04", 1) == 1);
+		
+		assertTrue(game.getCoordinateData("A5", 1) == 1);
+		assertTrue(game.getCoordinateData("b05", 1) == 1);
+		assertTrue(game.getCoordinateData("C5", 1) == 1);
+		assertTrue(game.getCoordinateData("d05", 1) == 1);
+		
+		assertTrue(game.getCoordinateData("A10", 1) == 1);
+		assertTrue(game.getCoordinateData("b10", 1) == 1);
+		assertTrue(game.getCoordinateData("C10", 1) == 1);
+		assertTrue(game.getCoordinateData("d10", 1) == 1);
+		assertTrue(game.getCoordinateData("E10", 1) == 1);
+		
 		
 	}
 
