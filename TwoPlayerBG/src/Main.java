aed2d75fee90f894e5e9f3ff78ff849eba799030
Polewarc323 <89818249+Polewarc323@users.noreply.github.com>
2022-03-02 12:37:35 -0500

    Docs

--- TwoPlayerBG/src/Main.java
+++ TwoPlayerBG/src/Main.java
@@ -1,6 +1,14 @@
-
+/****************************************************************************
+*
+This is the Main Class to start the program
+*
+*@author Christopher Polewarczyk
+*@version January 25, 2022
+*
+*
+*****************************************************************************/
 public class Main {
-	
+	/**Creates a new Hub to give the user to choose and play games*/
 	public static void main(String[] args) {	
 		
 		HubGUI HubGUI = new HubGUI();
